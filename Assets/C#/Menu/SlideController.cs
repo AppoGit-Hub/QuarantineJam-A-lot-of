28cb8542e5a8057601d33c8e7b48366001f47d2d
AppoGit-Hub <bl@dtd.be>
2020-03-16 15:55:32 +0100

    Release Commit

---
+++ Assets/C#/Menu/SlideController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlideController : MonoBehaviour
+{
+    [SerializeField] private Slider Slider;
+    [SerializeField] private Transform Panel;
+
+    private int Width = 12360;
+
+    public void ValueChanged(float n)
+    {
+        Panel.transform.localPosition = new Vector3(-(Width * n), 0, 0);
+    }
+
+    private void Update()
+    {
+        ValueChanged(Slider.value);
+    }
+}
---
+++ Assets/C#/ScreenShot/ScreenShot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShot : MonoBehaviour
+{
+    [SerializeField] private Camera Camera;
+
+    private bool TakeScreenShot = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F12))
+        {
+            TakeScreeShot();
+        }
+    }
+
+    private void OnPostRender()
+    {
+        if (TakeScreenShot)
+        {
+            TakeScreenShot = false;
+
+            RenderTexture t = Camera.targetTexture;
+
+            Texture2D renderResult = new Texture2D(1920, 1080);
+            Rect r = new Rect(0, 0, 1920, 1080);
+            renderResult.ReadPixels(r, 0, 0);
+
+            byte[] bytearray = renderResult.EncodeToPNG();
+            System.IO.File.WriteAllBytes("D:", bytearray);
+
+            //RenderTexture.ReleaseTemporary(t);
+            //Camera.targetTexture = null;
+        }
+    }
+
+    private void TakeScreeShot()
+    {
+        Camera.targetTexture = RenderTexture.GetTemporary(1920, 1080, 16);
+        TakeScreenShot = true;
+    }
+}
